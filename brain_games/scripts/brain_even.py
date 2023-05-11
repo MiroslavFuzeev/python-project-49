ef26ee159bf4e5aa1ad0aabd3cd679ab08de0d83
Miroslav Fuzeev <m.fuzeev@yandex.ru>
2023-05-11 16:16:41 +0300

    .

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -6,7 +6,6 @@ def greet():
 
 greet()
 
-
 def welcome_user():
     name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
@@ -14,33 +13,30 @@ def welcome_user():
 
 welcome_user()
 
-
 print("""Answer "yes" if the number is even, otherwise answer "no".""")
 
-def is_even ():
+name = welcome_user()
+
+def get_question_answer():
   random_num = randint(1, 100)
   print(f"Question: {random_num}")
   answ = prompt.string("Your answer: ")
-  count = 0
   if random_num % 2 == 0:
     if answ == 'yes':
-      count += 1
       print("Correct!")
-      is_even ()
     if answ == 'no':
       print("""'no' is wrong answer ;(. Correct answer was 'yes'.""")
       print(f"Let's try again, {name}!")
   if random_num % 2 != 0:
     if answ == 'no':
-      count += 1
       print("Correct!")
-      is_even ()
     if answ == 'yes':
       print("""'yes' is wrong answer ;(. Correct answer was 'no'.""")
       print(f"Let's try again, {name}!")
-  for i in range(3):
-    if count == 3:
-      print("Congratulations, Bill!")
 
 
-is_even ()
+count = 0
+while count < 3:
+  get_question_answer()
+  count += 1
+print((f"Congratulations, {name}!"))
